2e13a69c1ada122472af93a802213e5697950dcf
Yilmaz <yilmaz.koeknar@code.berlin>
2020-02-18 14:25:19 +0100

    initial commit

---
+++ src/HomeScreenComponents/SendLoveScreen.jsx
@@ -0,0 +1,20 @@
+import 'SendLoveStyles.css'
+import React from "react";
+
+class SendLove extends React.Component{
+    constructor(props) {
+        super (props)
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
+
+export default SendLove
