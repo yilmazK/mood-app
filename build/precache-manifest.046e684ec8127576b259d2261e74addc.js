297fbc8498926a58aecf7287f068c3f44e4d4a84
Yilmaz <yilmaz.koeknar@code.berlin>
2020-02-22 22:29:53 +0100

    h9

---
+++ build/precache-manifest.046e684ec8127576b259d2261e74addc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b03613496dc10d02bdb1b6e84e53d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19a0bdaa63af26bb7c7c",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "94d58c86a9f37b961709",
+    "url": "/static/css/main.d7575b9c.chunk.css"
+  },
+  {
+    "revision": "19a0bdaa63af26bb7c7c",
+    "url": "/static/js/2.5d3f4e74.chunk.js"
+  },
+  {
+    "revision": "499753d71e6ea6d5c381ad2f3b10d056",
+    "url": "/static/js/2.5d3f4e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d58c86a9f37b961709",
+    "url": "/static/js/main.ed8086c4.chunk.js"
+  },
+  {
+    "revision": "fc4c3fb2b7e94441e1e8",
+    "url": "/static/js/runtime-main.e2a0273a.js"
+  }
+]);
--- src/Backend/server.js
+++ src/Backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'build')));
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve('/public', 'build', 'index.html'))
+    res.sendFile(path.resolve('/', 'build', 'index.html'))
 })
 
 //Connect to mongoDB
