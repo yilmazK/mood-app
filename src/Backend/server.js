8d120277c67232496beb71ec60d8b47e5aa47db1
Yilmaz <yilmaz.koeknar@code.berlin>
2020-02-22 22:18:18 +0100

    h6

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'build')));
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    res.sendFile(path.resolve('/src/Frontend', 'build', 'index.html'))
 })
 
 //Connect to mongoDB
