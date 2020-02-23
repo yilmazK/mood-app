7678c5243ee58d64cf8a91cac63c806d089229ab
Yilmaz <yilmaz.koeknar@code.berlin>
2020-02-24 00:01:30 +0100

    fixed margin

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 30000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
