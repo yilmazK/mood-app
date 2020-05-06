d94959e0f57343ffb5902b77acada8cd67728860
Yilmaz <yilmaz.koeknar@code.berlin>
2020-05-06 15:34:24 +0200

    .env

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../build')));
 
 //Connect to mongoDB
-const uri = process.env.ATLAS_URI
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
